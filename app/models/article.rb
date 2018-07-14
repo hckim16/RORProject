7ed9dc0f2e52b1f34e3076adac22555c73f9a67a
Hyo Chang Kim <hck@hyochangkim.com>
2018-07-13 20:16:07 -0400

    Added search function

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,9 @@ class Article < ApplicationRecord
               length: {minimum: 5}
     validates :text, presence: true,
               length: {minimum: 2}
+
+    def self.search(params)
+        articles = Article.where("text LIKE ? or title LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
+        articles
+    end
 end
